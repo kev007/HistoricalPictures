c5dbec82a23e5eddac54b37e003ea3fff6b0b506
Kevin Shrestha <kev_shrestha@hotmail.com>
2018-08-08 21:03:42 +0200

    added meaningful error messages in the GUI after upload

--- controllers/upload.js
+++ controllers/upload.js
@@ -4,37 +4,52 @@ const User = require('../models/User');
 const Gamification = require('./applicationLogic/gamification.js');
 
 
-function saveFilesForUser(files, userId) {
-  User.findById(userId, (err, user) => {
-    if (err) { return err; }
+function saveFile(file, user) {
+  return new Promise((resolve, reject) => {
+    const status = {
+      name: file.originalname,
+      saved: false,
+      messages: []
+    };
 
-    files.forEach((file) => {
-      try {
-        new ExifImage({image: `./uploads/${file.filename}`}, function (error, exifData) {
-          if (error) {
-            console.log('Error: ' + error.message);
-          } else {
-            //create new Upload object
-            const upload = new Upload({
-              filename: file.filename,
-              user: user._id,
-              file,
-              exif: exifData
-            });
+    new ExifImage({image: `./uploads/${file.filename}`}, function (error, exifData) {
+      if (error) status.messages.push(error);
 
-            //save created object
-            upload.save((err) => {
-              if (err) { return err; }
-            });
+      //create new Upload object
+      const upload = new Upload({
+        filename: file.filename,
+        user: user._id,
+        file,
+        exif: exifData
+      });
 
-            //reward user, then save
-            Gamification.rewardUserUploads(user, files);
-            user.save();
-          }
-        });
-      } catch (error) {
-        console.log('Error: ' + error.message);
-      }
+      //save created object
+      upload.save((error) => {
+        if (error) status.messages.push(error);
+        else status.saved = true;
+        resolve(status)
+      });
+    });
+  });
+}
+
+function saveAllFilesForUser(files, userId) {
+  return new Promise((resolve, reject) => {
+    const promises = [];
+
+    User.findById(userId, (error, user) => {
+      if (error) reject(error);
+
+      files.forEach((file) => {
+        promises.push(saveFile(file, user));
+      });
+
+      Promise.all(promises).then(function (values) {
+        //reward user, then save
+        Gamification.rewardUserUploads(user, files);
+        user.save();
+        resolve(values);
+      });
     });
   });
 }
@@ -60,8 +75,21 @@ exports.postFileUpload = (req, res) => {
     return res.redirect('/picture/upload');
   }
 
-  saveFilesForUser(req.files, req.user.id);
-
-  req.flash('success', { msg: `${req.files.length} files uploaded successfully.` });
-  res.redirect('/picture/upload');
+  saveAllFilesForUser(req.files, req.user.id).then(function (resolved, reject) {
+    //display upload messages
+    resolved.forEach((status) => {
+      if (status.saved && status.messages.length === 0) {
+        req.flash('success', { msg: `Uploaded successfully: ${status.name}` });
+      } else if (status.saved && status.messages.length > 0) {
+        status.messages.forEach((message) => {
+          req.flash('info', { msg: `${status.name} was uploaded with warning - ${message.toString()}` });
+        });
+      }
+      if (!status.saved) {
+        req.flash('errors', { msg: `${status.name} uploaded with errors.` });
+        //TODO: check if it's in the filesystem, then delete?
+      }
+    });
+    res.redirect('/picture/upload');
+  });
 };
