f66db139cefb48d08f0dd9fc24da9ded42038cf7
kev007 <kev_shrestha@hotmail.com>
2018-08-28 13:16:37 +0200

    new data model for buildings

---
+++ models/Building_Tag.js
@@ -0,0 +1,42 @@
+// Reference: https://schema.org/GeoCoordinates
+const mongoose = require('mongoose');
+
+const buildingSchema = new mongoose.Schema({
+  id: { type: Number, unique: true },
+
+  deleted: { type: Boolean, default: false },
+  readonly: { type: Boolean, default: false },
+  version: {
+    number: Number,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  },
+
+  // all the pictures that contain this tag
+  pictures: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Picture' }],
+
+  name: { type: mongoose.Schema.Types.ObjectId, ref: 'Name' },
+  geo: {
+    latitude: Number,
+    longitude: Number,
+    elevation: Number,
+    street: String,
+    house_number: String,
+    postalCode: Number,
+    addressCountry: { type: String, default: "DE" }
+  },
+  time: {
+    built: { type: Date },
+    builtAlt: { type: Date },
+    unbuilt: { type: Date },
+    unbuiltAlt: { type: Date },
+    unbuilt_type: { type: mongoose.Schema.Types.ObjectId, ref: 'Tag' },
+  }
+}, {
+  timestamps: true,
+  versionKey: true
+});
+
+
+const Building_Tag = mongoose.model('Building', buildingSchema);
+
+module.exports = Building_Tag;
